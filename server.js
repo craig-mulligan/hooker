56a37bff86ac62fb405e377a9ea1a780e89c5218
Craig Mulligan <craig@firingsuad.co.za>
2015-06-06 08:20:45 +0100

    pm2

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-var util = require('util'),  
-    http = require('http');
-    port = 8080
+//Lets require/import the HTTP module
+var http = require('http');
 
-http.createServer(function (req, res) {  
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Forever? forever eva? forever eva?')
-  res.end();
-  console.log("Server listening on: http://localhost:%s", port);
-}).listen(port);
+//Lets define a port we want to listen to
+const PORT=8080; 
 
-// /* server started */  
-// util.puts('> hello world running on port 8080');
+//We need a function which handles requests and send response
+function handleRequest(request, response){
+    response.end('It Works!! Path Hit: ' + request.url);
+}
 
-// setTimeout(function () {  
-//   util.puts('Throwing error now.');
-//   throw new Error('User generated fault.');
-// }, 5000);
+//Create a server
+var server = http.createServer(handleRequest);
+
+//Lets start our server
+server.listen(PORT, function(){
+    //Callback triggered when server is successfully listening. Hurray!
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
