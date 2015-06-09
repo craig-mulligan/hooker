97a1945f1921d56f9b19c388eb5161dd1bb1912d
Craig Mulligan <craig@firingsuad.co.za>
2015-06-09 14:24:28 +0100

    another test

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var execOptions = {
      maxBuffer: 1024 * 1024 // 1mb
 }
 
-setInterval(function() {  console.log("oops!"); }, 2000);
+setInterval(function() {  console.log("oopsdaisy!"); }, 2000);
 
 gith({
     repo: 'craig-mulligan/hooker'
