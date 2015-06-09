736dc940fae815322395f4de3a74b16ba667d8f5
Craig Mulligan <craig@firingsuad.co.za>
2015-06-09 19:28:00 +0100

    heyo

--- watcher.js
+++ watcher.js
@@ -7,7 +7,7 @@ var execOptions = {
      maxBuffer: 1024 * 1024 // 1mb
 }
 
-setInterval(function() {  console.log("tits!"); }, 3000);
+setInterval(function() {  console.log("flip!"); }, 4000);
 
 gith({
     repo: 'craig-mulligan/hooker'
