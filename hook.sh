e0c1c047f7af663cf3a2e28ab770d3512d005b56
Craig Mulligan <craig@firingsuad.co.za>
2015-06-09 18:30:17 +0100

    heyo

--- hook.sh
+++ hook.sh
@@ -1,3 +1,3 @@
 #!/bin/bash  
 echo "testicle"
-cd /app && git fetch && git reset --hard origin/master && /usr/local/bin/node server.js
+cd /app && git fetch && git reset --hard origin/master && /usr/local/bin/node watcher.js
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 cd /app && git init && git remote add origin https://github.com/craig-mulligan/hooker.git 
 chmod 777 "/app/hook.sh"
-cd /app && /usr/local/bin/node watcher.js && /usr/local/bin/node server.js
+cd /app forever start watcher.js
