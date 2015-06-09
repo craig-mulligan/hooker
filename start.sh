03cf2d6598a625fa1baa281b1d6b743d8e03215e
Craig Mulligan <craig@firingsuad.co.za>
2015-06-09 18:47:10 +0100

    heyo

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 cd /app && git init && git remote add origin https://github.com/craig-mulligan/hooker.git 
 chmod 777 "/app/hook.sh"
-cd /app forever start watcher.js
+cd /app && forever start watcher.js
