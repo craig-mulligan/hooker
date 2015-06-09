281c6ca0d08fa97c1add1834b3b4f8a8f05d3c96
Craig Mulligan <craig@firingsuad.co.za>
2015-06-09 18:32:30 +0100

    heyo

---
+++ watcher.js
@@ -0,0 +1,25 @@
+// Listen on port 9001
+var gith = require('gith').create(8080);
+// Import execFile, to run our bash script
+var execFile = require('child_process').execFile;
+
+var execOptions = {
+     maxBuffer: 1024 * 1024 // 1mb
+}
+
+gith({
+    repo: 'craig-mulligan/hooker'
+}).on( 'all', function( payload ) {
+    if( payload.branch === 'master' )
+        console.log("master action triggered")
+    {
+            // Exec a shell script
+            execFile('/app/hook.sh', execOptions, function(error, stdout, stderr) {
+                    // Log success in some manner
+                    console.log( 'exec complete test' );
+                    if(error){ console.log(error); } 
+                    console.log('stdout:\t',stdout); 
+                    console.log('stderr:\t',stderr); 
+            });
+    }
+});
